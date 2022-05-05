0ea0b34f1c85606b3c0843fca02fcbf3555ec513
samyhaff <haffoudhisamy@gmail.com>
2022-05-05 20:50:50 +0200

    added icons

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" MEM ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
-    {"DISP ", "echo $(( $(cat /sys/class/backlight/amdgpu_bl0/brightness) * 100 / 255 ))", 5, 0},
-    {"VOL ", "pulsemixer --get-volume | awk '{print $1;}'", 5, 0},
-    {"WIFI ", "iwgetid -r", 5, 0},
-    {"BAT ", "cat /sys/class/power_supply/BAT0/capacity", 5, 0},
-	{"", "date '+%b %d (%a) %I:%M%p'", 5, 0},
+	{"   ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {"  ", "printf '%d%%' $(echo $(( $(cat /sys/class/backlight/amdgpu_bl0/brightness) * 100 / 255 )))", 5, 0},
+    {"  ", "printf '%d%%' $(pulsemixer --get-volume | awk '{print $1;}')", 5, 0},
+    {"  ", "iwgetid -r", 5, 0},
+    {"   ", "printf '%d%%' $(cat /sys/class/power_supply/BAT0/capacity)", 5, 0},
+	{"  ", "date '+%b %d (%a) %H:%M '", 5, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
