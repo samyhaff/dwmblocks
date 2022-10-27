d4aa26e74f38d1e4d5063b2e04cd6aaff13e0460
samyhaff <haffoudhisamy@gmail.com>
2022-10-27 22:25:23 +0200

    added colors

--- blocks.def.h
+++ blocks.def.h
@@ -3,11 +3,11 @@ static const Block blocks[] = {
 	/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
 
 	// {"   ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
-    {"   ", "printf '%d%%' $(echo $(( $(cat /sys/class/backlight/amdgpu_bl0/brightness) * 100 / 255 )))", 1, 0},
+    {"^C3^   ^d^", "printf '%d%%' $(echo $(( $(cat /sys/class/backlight/amdgpu_bl0/brightness) * 100 / 255 )))", 1, 0},
     {"", "status_volume", 1, 0},
     {"", "status_wifi", 1, 0},
     {"", "status_battery", 1, 0},
-	{"  ", "date '+%b %d (%a) %H:%M '", 1, 0},
+	{"^C1^  ^d^", "date '+%b %d (%a) %H:%M '", 1, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
