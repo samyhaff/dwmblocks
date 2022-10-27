728dc2508bc9b805a1c303c9f8b3be3273dbc46c
samyhaff <haffoudhisamy@gmail.com>
2022-10-27 18:50:09 +0200

    added scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+
 	// {"   ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
-    {"   ", "printf '%d%%' $(echo $(( $(cat /sys/class/backlight/amdgpu_bl0/brightness) * 100 / 255 )))", 5, 0},
-    {"  ", "printf '%d%%' $(pulsemixer --get-volume | awk '{print $1;}')", 5, 0},
-    {"  ", "iwgetid -r", 5, 0},
-    {"   ", "printf '%d%%' $(cat /sys/class/power_supply/BAT0/capacity)", 5, 0},
-	{"  ", "date '+%b %d (%a) %H:%M '", 5, 0},
+    {"   ", "printf '%d%%' $(echo $(( $(cat /sys/class/backlight/amdgpu_bl0/brightness) * 100 / 255 )))", 1, 0},
+    {"", "status_volume", 1, 0},
+    {"", "status_wifi", 1, 0},
+    {"", "status_battery", 1, 0},
+	{"  ", "date '+%b %d (%a) %H:%M '", 1, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
